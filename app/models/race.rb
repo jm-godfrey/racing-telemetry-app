71f64d8859be8b3c26de2b540e4f665d0432fc4e
James Godfrey <jgodfrey2@sheffield.ac.uk>
2026-06-16 04:01:21 +0100

    Annotate Race model and factory

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: races
+#
+#  id                 :bigint           not null, primary key
+#  duration_ms        :integer
+#  lap_count          :integer          default(0), not null
+#  name               :string           not null
+#  recorded_at        :datetime
+#  sample_count       :integer          default(0), not null
+#  start_finish_lat_a :float
+#  start_finish_lat_b :float
+#  start_finish_lon_a :float
+#  start_finish_lon_b :float
+#  status             :integer          default("pending"), not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 class Race < ApplicationRecord
   has_one_attached :csv_file
 
